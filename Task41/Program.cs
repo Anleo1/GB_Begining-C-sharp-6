bd1c171026db47f21295f9c5a1bc9401389c1b14
Anastasia <anastpcher@rambler.ru>
2022-12-14 23:31:07 +0300

    Sixth task

---
+++ Task41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите числа (чтобы закончить ввод, нажмите пробел и enter): ");
+int count = 0;
+string num_st = "";
+while (num_st != " ") {
+    num_st = Console.ReadLine();
+    if (num_st == " "){
+        break;
+    }
+    if (Convert.ToDouble(num_st) > 0) count++;
+} 
+Console.WriteLine($"Количество чисел больших нуля = {count}");
+
---
+++ Task43/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите аргументы уравнения первой прямой");
+Console.Write("K1 = ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b1 = ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите аргументы уравнения второй прямой");
+Console.Write("K2 = ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 = ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2){
+    if (b1 == b2){
+        Console.WriteLine("Прямые совпадают");
+        return 0;
+    }
+    else {
+        Console.WriteLine("Прямые не пересекаются");
+        return 0;
+    }
+}
+else {
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения : ({x}; {y})");
+    return 0;
+}
